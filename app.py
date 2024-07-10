4c5742ffcd60902502108aeec60596fc4dc3ebca
WiseStar282 <87527087+WiseStar282@users.noreply.github.com>
2024-07-10 17:46:59 +0700

    tambahkan integrasi sistem
    membuat app.py & menggabungkan dengan model regresi

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model
+with open('./model/model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Ambil data dari form
+    lb = request.form['lb']
+    lt = request.form['lt']
+    kt = request.form['kt']
+    km = request.form['km']
+    grs = request.form['grs']
+
+    # Buat array untuk prediksi
+    input_data = np.array([[lb, lt, kt, km, grs]], dtype=float)
+
+    # Prediksi
+    prediction = model.predict(input_data)[0]
+    prediction_milyar = prediction / 1000  # Konversi ke milyar
+
+    return render_template('index.html', prediction_text=f'Prediksi harga rumah adalah: {prediction_milyar:.2f} milyar')
+
+if __name__ == '__main__':
+    app.run(debug=True)

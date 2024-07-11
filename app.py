d562cc65fe11abb9886ef8cc14f05a800e091227
HijazP <hanifaliramdani98@gmail.com>
2024-07-11 22:47:42 +0700

    makes css inside html (stress dengan cors)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 # Load model
 with open('./model/model.pkl', 'rb') as f:
